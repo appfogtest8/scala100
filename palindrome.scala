ca70aa8f12ac7ed339c24678b1a45fa72c134116
appfogtest8 <appfogtest8@mm.st>
2013-12-01 04:13:45 -0800

    init commit

---
+++ palindrome.scala
@@ -0,0 +1,9 @@
+var x = readLine("? ")
+
+var b = x.toLowerCase.toList.filter((i: Char) => i != ' ')
+
+if ((b take (b.length / 2)) == (b.reverse take (b.length / 2))){
+	println("yes");
+	}else{
+	println("no");
+}
